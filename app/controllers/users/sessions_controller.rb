565339af246726d04bc65a37acbad9e864a4c19f
Saif-041 <sp19-bse-041@cuilahore.edu.pk>
2022-08-15 14:57:09 +0500

    checking log out.
    checking log out.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,16 +9,16 @@ class Users::SessionsController < Devise::SessionsController
     private
     def respond_with(resource, _opts = {})
 
-    render json: { message: 'Logged in successfully.', status: 200,data: resource }, status: :ok
+    render json: { message: 'Logged in successfully.', status: 200, data: resource }, status: :ok
     end
     def respond_to_on_destroy
-      current_user.present? ? log_out_failure : log_out_success 
+      current_user.present? ? log_out_success : log_out_failure
     end
     def log_out_success
       render json: { message: "Logged out sucessfully.", status: 200 }, status: :ok
     end
     def log_out_failure
-      render json: { message: "Logged out failure.", status: 500}, status: :unauthorized
+      render json: { message: "Logged out failure.", status: 500 }, status: :unauthorized
     end
 
     #def jsonResponse
