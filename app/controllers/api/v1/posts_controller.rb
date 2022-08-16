48a130c8daf375c1509cba30e835b6693b1a018f
Saif-041 <sp19-bse-041@cuilahore.edu.pk>
2022-08-16 18:42:29 +0500

    no  change
    no  change.

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::PostsController < ApplicationController
+  ## To fix the problem Can't verify CSRF token authenticity.
+  # uncomment below line.
+  #  skip_before_action :verify_authenticity_token
+
   before_action :authenticate_user!
   before_action :set_api_v1_post, only: %i[ show update destroy ]
 
