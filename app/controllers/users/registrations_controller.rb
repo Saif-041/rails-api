99f5e39b8c52aedeab841ef5001844365782d333
Saif-041 <sp19-bse-041@cuilahore.edu.pk>
2022-08-16 12:48:33 +0500

    registration ok req for all time
    registration ok req for all time.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,14 +1,19 @@
 class Users::RegistrationsController < Devise::RegistrationsController
     respond_to :json
+    before_action :authenticate_user!
     # flash.now[:message] = "Hello Usman Sir"
     private
+
     def respond_with(resource, _opts = {})
       resource.persisted? ? register_success : register_failed
     end
+
     def register_success
-      render json: { message: 'Signed up.', status: 200 }
+      render json: { message: 'Signed up suucessfully!', status: 200 }, status: :ok
     end
+
     def register_failed
-      render json: { message: "Signed up failure.", status: 500 }
+      render json: { message: "User Already Exists.", status: 500 }, status: :ok
     end
+
   end
